82492f698123d017d2e0b0d7e56a59de239afa86
Scott Aubrey <scott@aubrey.org.uk>
2022-10-12 09:33:10 +0100

    fix(validate.sh): attempt an envsubst to see if we can resolve the issue with int/string validation

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -60,7 +60,7 @@ echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" $kustomize_flags | \
+    kustomize build "${file/%$kustomize_config}" $kustomize_flags | envsubst | \
       kubeconform $kubeconform_config
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
