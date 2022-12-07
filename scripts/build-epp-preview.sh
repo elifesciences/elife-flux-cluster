3e92fca9de793a0580624e91f64c758b124583ba
Scott Aubrey <scott@aubrey.org.uk>
2022-12-07 09:34:01 +0000

    EPP preview deployment (#891)
    * create a build-epp-preview script
    
    * receive pr_id as a parameter
    
    * add passing in server and client image
    
    * attempt to create a preview PR generator action

---
+++ scripts/build-epp-preview.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+re='^[0-9]+$'
+if ! [[ $1  =~ $re ]]; then
+    echo "first param must be a PR number"
+    exit 1
+fi
+pr_id=$1
+
+if [[ -z $2 ]]; then
+    echo "second param must be the server image tag"
+    exit 1
+fi
+servertag=$2
+
+if [[ -z $3 ]]; then
+    echo "second param must be the server image tag"
+    exit 1
+fi
+clienttag=$3
+
+echo "pr_id: $pr_id, server: $servertag, client: $clienttag, image: $imagesev"
+
+PREVIEWS_DIR='deployments/epp/previews'
+ENV_NAME_PREFIX='epp-preview'
+HOSTNAME_SUFFIX='epp'
+ORG='elifesciences'
+KUSTOMIZATION_TEMPLATE='kustomizations/apps/epp/preview_template.yaml'
+
+# export image_tag="preview-${pr_commit:0:8}"
+export deployment_name="$ENV_NAME_PREFIX-${pr_id}"
+export deployment_hostname="pr-${pr_id}--${HOSTNAME_SUFFIX}.elifesciences.org"
+
+ENV_DEST_DIR="${PREVIEWS_DIR}/$pr_id"
+echo "Creating env for PR $pr_id at ${ENV_DEST_DIR}"
+mkdir -p "${ENV_DEST_DIR}" | true
+
+echo "create namespace at ${ENV_DEST_DIR}/namespace.yaml"
+yq ".metadata.name = \"${deployment_name}\"" kustomizations/apps/epp/preview/namespace_template.yaml > ${ENV_DEST_DIR}/namespace.yaml
+
+echo "create database at ${ENV_DEST_DIR}/mongodb-release.yaml "
+yq ".metadata.namespace = \"${deployment_name}\" | .metadata.labels.app_env = \"${deployment_name}\"" kustomizations/apps/epp/preview/mongodb-release_template.yaml > ${ENV_DEST_DIR}/mongodb-release.yaml
+
+echo "create kustomization at ${ENV_DEST_DIR}/epp-kustomization.yaml "
+cp kustomizations/apps/epp/preview/epp-kustomization_template.yaml ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i ".metadata.namespace = \"${deployment_name}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i ".spec.targetNamespace = \"${deployment_name}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i ".spec.postBuild.substitute.app_env = \"${deployment_name}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i ".spec.postBuild.substitute.app_hostname = \"${deployment_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i ".spec.postBuild.substitute.asset_prefix = \"https://${deployment_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i ".spec.postBuild.substitute.iiif_server = \"https://${deployment_hostname}/iiif\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+
+# replace image tags
+yq -i "(.spec.images[] | select(.name == \"ghcr.io/elifesciences/enhanced-preprints\") | .value) = \"${servertag}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+yq -i "(.spec.images[] | select(.name == \"ghcr.io/elifesciences/enhanced-preprints-client\") | .value) = \"${clienttag}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
