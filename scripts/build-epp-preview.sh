f9ab1588d2706747d447cbf161e7fd970a063054
Scott Aubrey <scott@aubrey.org.uk>
2023-03-24 07:57:38 +0000

    Fix previews to only deploy client and storybook

--- scripts/build-epp-preview.sh
+++ scripts/build-epp-preview.sh
@@ -48,8 +48,8 @@ mkdir -p "${ENV_DEST_DIR}" | true
 echo "create namespace at ${ENV_DEST_DIR}/namespace.yaml"
 yq ".metadata.name = \"${deployment_name}\"" kustomizations/apps/epp/preview/namespace_template.yaml > ${ENV_DEST_DIR}/namespace.yaml
 
-echo "create database at ${ENV_DEST_DIR}/mongodb-release.yaml "
-yq ".metadata.namespace = \"${deployment_name}\" | .metadata.labels.app_env = \"${deployment_name}\"" kustomizations/apps/epp/preview/mongodb-release_template.yaml > ${ENV_DEST_DIR}/mongodb-release.yaml
+# echo "create database at ${ENV_DEST_DIR}/mongodb-release.yaml "
+# yq ".metadata.namespace = \"${deployment_name}\" | .metadata.labels.app_env = \"${deployment_name}\"" kustomizations/apps/epp/preview/mongodb-release_template.yaml > ${ENV_DEST_DIR}/mongodb-release.yaml
 
 echo "create kustomization at ${ENV_DEST_DIR}/epp-kustomization.yaml "
 cp kustomizations/apps/epp/preview/epp-kustomization_template.yaml ${ENV_DEST_DIR}/epp-kustomization.yaml
@@ -58,11 +58,11 @@ yq -i ".spec.targetNamespace = \"${deployment_name}\"" ${ENV_DEST_DIR}/epp-kusto
 yq -i ".spec.postBuild.substitute.app_env = \"${deployment_name}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
 yq -i ".spec.postBuild.substitute.app_hostname = \"${deployment_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
 yq -i ".spec.postBuild.substitute.storybook_hostname = \"${deployment_storybook_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
-yq -i ".spec.postBuild.substitute.journal_api_hostname = \"${deployment_journal_api_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+# yq -i ".spec.postBuild.substitute.journal_api_hostname = \"${deployment_journal_api_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
 yq -i ".spec.postBuild.substitute.asset_prefix = \"https://${deployment_hostname}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
-yq -i ".spec.postBuild.substitute.iiif_server = \"https://${deployment_hostname}/iiif\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+# yq -i ".spec.postBuild.substitute.iiif_server = \"https://${deployment_hostname}/iiif\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
 
 # replace image tags
-yq -i "(.spec.images[] | select(.name == \"ghcr.io/elifesciences/enhanced-preprints\") | .newTag) = \"${servertag}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
+# yq -i "(.spec.images[] | select(.name == \"ghcr.io/elifesciences/enhanced-preprints\") | .newTag) = \"${servertag}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
 yq -i "(.spec.images[] | select(.name == \"ghcr.io/elifesciences/enhanced-preprints-client\") | .newTag) = \"${clienttag}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
 yq -i "(.spec.images[] | select(.name == \"ghcr.io/elifesciences/enhanced-preprints-storybook\") | .newTag) = \"${storybooktag}\"" ${ENV_DEST_DIR}/epp-kustomization.yaml
