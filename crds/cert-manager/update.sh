2197025476f08bc2a5e541cb41336a0cb5a06378
Scott Aubrey <scott@aubrey.org.uk>
2023-06-29 09:04:52 +0100

    fix(crds): update cert-manager script to use the templated output instead

--- crds/cert-manager/update.sh
+++ crds/cert-manager/update.sh
@@ -2,12 +2,10 @@
 
 # Get current cert-manager chart
 
+release_name=$(yq '.spec.releaseName' ../../system/infra/cert-manager/release.yaml)
 chart_name=$(yq '.spec.chart.spec.chart' ../../system/infra/cert-manager/release.yaml)
 chart_version=$(yq '.spec.chart.spec.version' ../../system/infra/cert-manager/release.yaml)
 chart_repo=$(yq '.spec.url' ../../system/infra/cert-manager/source.yaml)
+echo "$chart_repo" "$release_name" "$chart_name" "$chart_version"
 
-outputdir=$(mktemp -d)
-helm pull --repo "$chart_repo" "$chart_name" --version "$chart_version" --untar --untardir $outputdir
-cp $outputdir/cert-manager/templates/crds.yaml ./cert-manager.crds.yaml
-
-rm -R $outputdir
+helm template --repo "$chart_repo" "$release_name" "$chart_name" --version "$chart_version" --set installCRDs=true | yq 'select(.kind == "CustomResourceDefinition")' > ./cert-manager.crds.yaml
