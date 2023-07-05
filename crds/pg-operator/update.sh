a1c9b741d5b10e7c7f07802ee7043fa9937f6354
Scott Aubrey <scott@aubrey.org.uk>
2023-07-05 19:11:13 +0100

    deploy(crds): track CRDs for latest db operators, and create an update script for each (#2201)

---
+++ crds/pg-operator/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a psmdb-operator helm chart git reference to get the CRDs'
+    echo "e.g. \`$0 1.12.0\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -O https://raw.githubusercontent.com/percona/percona-helm-charts/pg-operator-$1/charts/pg-operator/crds/crd.yaml
---
+++ crds/pxc-operator/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a psmdb-operator helm chart git reference to get the CRDs'
+    echo "e.g. \`$0 1.12.0\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -O https://raw.githubusercontent.com/percona/percona-helm-charts/pxc-operator-$1/charts/pxc-operator/crds/crd.yaml
