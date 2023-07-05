6f153275daabd23a10707ec0ec364be5ff316b27
Scott Aubrey <scott@aubrey.org.uk>
2023-07-05 20:10:51 +0100

    feat(workflows/e2e): Run workloads using kwok (#1778)
    * Alter the CI to use a script that creates a cluster in KinD, adds a fake node using Kwok, and tests all deployments are OK
    
    * add execute bit to script
    
    * make configurable
    
    * fix configured repo URL
    
    * update config again to the correct repo, and add more checks for deployments
    
    * reduce scope for now
    
    * fix node selector name
    
    * Move resources to test external to the script
    
    * use correct field separator
    
    * switch tests around a bit and more sure we're including all deployments
    
    * give more timeout to deployments
    
    * reorganise test in order mostlikely to succeed quickly
    
    * add more to debug summary
    
    * add ephemeral storage to simulated node
    
    * reduce timeout
    
    * add more debugging
    
    * alter the certmanager and datahub databasees to deploy to the real node
    
    * try patching data-hub and cert manager again
    
    * try better patch
    
    * try another form of patch
    
    * try another form of patch
    
    * reduce scope away from full deployments again
    
    * try patches again
    
    * fix patch
    
    * fix patch
    
    * fix patch
    
    * fix patch
    
    * fix patch
    
    * fix patch
    
    * add required secrets to run nr daemonset
    
    * reduce KinD overhead
    
    * reduce overhead of data-hub resources
    
    * actually - just disable persistence to run on the big fake node
    
    * remove the kind reduction stuff
    
    * increase timeout
    
    * add install remediation
    
    * remove extra debugging that isn't required
    
    * add remediation
    
    * change test for valid resources
    
    * update kwok
    
    * update kind
    
    * fix kwok upgrade
    
    * misc fixes for running in e2e cluster
    
    * more fixes to attempt to get this up and running
    
    * remove unused patch

---
+++ scripts/run_cluster_in_kind.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+name="elife-flux-cluster"
+repo="https://github.com/elifesciences/elife-flux-cluster"
+test_kustomization_path="./clusters/end-to-end-tests"
+
+# if a param is specified, update the flux-system source branch to that
+branch=${1:-master}
+
+echo "Building KinD cluster using '$branch' branch"
+
+kind delete cluster --name "$name"
+kind create cluster --name "$name" --image=kindest/node:v1.27.3
+
+# install kwok into cluster
+kubectl kustomize scripts/kwok/deploy_config | kubectl apply -f -
+kubectl wait deployment -n kube-system kwok-controller --timeout=60s --for condition=Available=True
+
+# Install Flux with toleration to run controllers on the real node
+kubectl create ns flux
+# make sure flux components get installed, with additional components
+flux install --components-extra="image-reflector-controller,image-automation-controller" --toleration-keys=realnode
+
+# taint the current node to not schedule workloads by default
+kubectl taint node "$name-control-plane" realnode=true:NoSchedule
+# Install kwok nodes to run "workloads" on
+kubectl apply -f scripts/kwok/1_large_simulated_node.yaml
+
+
+
+# Preinstall
+# some components require a secret ahead of deployment
+kubectl create ns monitoring
+kubectl create secret -n monitoring generic newrelic-license --from-literal=prod=prod
+# Install cluster stuff and wait
+flux create source git flux-system --url="$repo" --branch="$branch"
+flux create kustomization flux-system --source=flux-system --path="$test_kustomization_path"
+kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=1m -n flux-system flux-system
+
+while IFS=$' ' read -r resource state namespace name ; do
+    if [[ "${resource:0:1}" == "#" ]]; then
+        continue;
+    fi
+    if [[ $resource == "" ]]; then
+        continue;
+    fi
+    kubectl wait "${resource}" --for=condition=${state} --timeout=5m -n ${namespace} ${name}
+done < "$test_kustomization_path/resources_to_test.txt"
