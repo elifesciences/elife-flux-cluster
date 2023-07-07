7fc31bf90c77442d6a14f8fdbbc37d2ce4176bc9
Scott Aubrey <scott@aubrey.org.uk>
2023-07-07 08:37:07 +0100

    attempt to fix occasional timeout with peerscout due to ingress not being up (#2202)
    * attempt to fix occasional timeout with peerscout due to ingress not being up
    
    * wait for sealed-secrets
    
    * add install remediation for ingress-nginx
    
    * try not waiting for ingress again
    
    * add extra debugging
    
    * give a longer timeout
    
    * move tests into their own folder
    
    * better name for step

--- tests/run_cluster_in_kind.sh
+++ tests/run_cluster_in_kind.sh
@@ -4,6 +4,7 @@ set -e
 name="elife-flux-cluster"
 repo="https://github.com/elifesciences/elife-flux-cluster"
 test_kustomization_path="./clusters/end-to-end-tests"
+tests_path="./tests"
 
 # if a param is specified, update the flux-system source branch to that
 branch=${1:-master}
@@ -14,7 +15,7 @@ kind delete cluster --name "$name"
 kind create cluster --name "$name" --image=kindest/node:v1.27.3
 
 # install kwok into cluster
-kubectl kustomize scripts/kwok/deploy_config | kubectl apply -f -
+kubectl kustomize $tests_path/kwok/deploy_config | kubectl apply -f -
 kubectl wait deployment -n kube-system kwok-controller --timeout=60s --for condition=Available=True
 
 # Install Flux with toleration to run controllers on the real node
@@ -25,7 +26,7 @@ flux install --components-extra="image-reflector-controller,image-automation-con
 # taint the current node to not schedule workloads by default
 kubectl taint node "$name-control-plane" realnode=true:NoSchedule
 # Install kwok nodes to run "workloads" on
-kubectl apply -f scripts/kwok/1_large_simulated_node.yaml
+kubectl apply -f $tests_path/kwok/1_large_simulated_node.yaml
 
 
 
@@ -37,13 +38,3 @@ kubectl create secret -n monitoring generic newrelic-license --from-literal=prod
 flux create source git flux-system --url="$repo" --branch="$branch"
 flux create kustomization flux-system --source=flux-system --path="$test_kustomization_path"
 kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=1m -n flux-system flux-system
-
-while IFS=$' ' read -r resource state namespace name ; do
-    if [[ "${resource:0:1}" == "#" ]]; then
-        continue;
-    fi
-    if [[ $resource == "" ]]; then
-        continue;
-    fi
-    kubectl wait "${resource}" --for=condition=${state} --timeout=5m -n ${namespace} ${name}
-done < "$test_kustomization_path/resources_to_test.txt"
