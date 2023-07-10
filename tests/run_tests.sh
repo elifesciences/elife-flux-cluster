c384b99e80e497a96fa9eef55ead14bb48c296fd
Scott Aubrey <scott@aubrey.org.uk>
2023-07-10 10:11:53 +0100

    reduce timeout

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,5 +9,5 @@ while IFS=$' ' read -r resource state namespace name ; do
     if [[ $resource == "" ]]; then
         continue;
     fi
-    kubectl wait "${resource}" --for=condition=${state} --timeout=10m -n ${namespace} ${name}
+    kubectl wait "${resource}" --for=condition=${state} --timeout=5m -n ${namespace} ${name}
 done < "$tests_path/resources_to_test.txt"
