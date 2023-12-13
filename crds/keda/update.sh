6ca30c896cf315cfe1a69d5fd0857ba7ca6055b4
Scott Aubrey <scott@aubrey.org.uk>
2023-12-13 11:41:16 +0000

    add keda crds

---
+++ crds/keda/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a keda release version to get the CRDs'
+    echo "e.g. \`$0 2.12.1\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -L https://github.com/kedacore/keda/releases/download/v$1/keda-$1-crds.yaml > keda.crd.yaml
