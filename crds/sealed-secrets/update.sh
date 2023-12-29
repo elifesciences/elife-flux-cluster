b4b04f00ceebcce7e5748a556c44b5e70f069db7
Scott Aubrey <scott@aubrey.org.uk>
2023-12-29 08:24:02 +0000

    add sealed-secrets CRDs

---
+++ crds/sealed-secrets/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a sealed-secrets helm chart git reference to get the CRDs'
+    echo "e.g. \`$0 v0.24.5\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -O https://raw.githubusercontent.com/bitnami-labs/sealed-secrets/$1/helm/sealed-secrets/crds/bitnami.com_sealedsecrets.yaml
