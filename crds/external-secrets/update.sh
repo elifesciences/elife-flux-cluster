1d869bff1a85dbb5bfd31800e78aa7c91e2b5e62
Scott Aubrey <scott@aubrey.org.uk>
2024-04-08 08:41:23 +0100

    feat(external-secrets): deploy external secrets operator

---
+++ crds/external-secrets/update.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify an external-secrets git reference to get the CRDs'
+    echo "e.g. \`$0 v0.9.9\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -L https://raw.githubusercontent.com/external-secrets/external-secrets/$1/deploy/crds/bundle.yaml > external-secrets.crds.yaml
