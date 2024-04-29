da16393e2f6feb67379b2ddecad72b74c529a8ea
Scott Aubrey <scott@aubrey.org.uk>
2024-04-29 09:06:58 +0100

    deploy(keda): deploy the keda-http-add-on

---
+++ crds/keda-http-add-on/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a keda-http-add-on release version to get the CRDs'
+    echo "e.g. \`$0 0.7.0\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -L https://github.com/kedacore/http-add-on/releases/download/v$1/keda-http-add-on-$1-crds.yaml > keda-http-add-on.crd.yaml
