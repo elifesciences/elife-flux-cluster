1c5868d4d70d9b58b529b5b6207f79342b907f13
Scott Aubrey <scott@aubrey.org.uk>
2024-04-29 06:37:52 +0100

    deploy(karpenter): add CRDs

---
+++ crds/karpenter/update.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a karpenter git reference to get the CRDs'
+    echo "e.g. \`$0 v0.36.1\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -O https://raw.githubusercontent.com/aws/karpenter-provider-aws/$1/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml
+curl -O https://raw.githubusercontent.com/aws/karpenter-provider-aws/$1/pkg/apis/crds/karpenter.sh_nodeclaims.yaml
+curl -O https://raw.githubusercontent.com/aws/karpenter-provider-aws/$1/pkg/apis/crds/karpenter.sh_nodepools.yaml
