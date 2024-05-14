51fcb513b55548290cac070fecb939420ddf1d40
Scott Aubrey <scott@aubrey.org.uk>
2024-05-14 16:26:00 +0100

    Fix ci e2e test (#2756)
    * try fix up the CI test suite on flux-prod
    
    * upgrade kind node version
    
    * fix system kustomization path
    
    * update resources to test
    
    * fix patches to select the correct node
    
    * use stern to display logs rather than kubectl logs randomly with --previous
    
    * import CI stuff from e2e workflow from flux-test
    
    * add missing secret for vm
    
    * disable testing deployments for now
    
    * disable deployments test
    
    * disable deployments test
    
    * remove more deployment related tests

--- tests/run_cluster_in_kind.sh
+++ tests/run_cluster_in_kind.sh
@@ -12,13 +12,15 @@ branch=${1:-master}
 echo "Building KinD cluster using '$branch' branch"
 
 kind delete cluster --name "$name"
-kind create cluster --name "$name" --image=kindest/node:v1.27.3
+kind create cluster --name "$name" --image=kindest/node:v1.28.9
 
 # install kwok into cluster
-KWOK_REPO=kubernetes-sigs/kwok
-KWOK_LATEST_RELEASE=$(curl "https://api.github.com/repos/${KWOK_REPO}/releases/latest" | jq -r '.tag_name')
-kubectl apply -f "https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/kwok.yaml"
-kubectl apply -f "https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/stage-fast.yaml"
+KWOK_RELEASE=v0.5.1
+kubectl apply -f "https://github.com/kubernetes-sigs/kwok/releases/download/${KWOK_RELEASE}/kwok.yaml"
+# patch kwok to run on the realnode
+kubectl patch deployment -n kube-system kwok-controller -p '{"spec":{"template":{"spec":{"tolerations":[{"key":"realnode","operator":"Equal","value":"true","effect":"NoSchedule"}]}}}}'
+kubectl apply -f "https://github.com/kubernetes-sigs/kwok/releases/download/${KWOK_RELEASE}/stage-fast.yaml"
+kubectl apply -f "https://github.com/kubernetes-sigs/kwok/releases/download/${KWOK_RELEASE}/metrics-usage.yaml"
 kubectl wait deployment -n kube-system kwok-controller --timeout=60s --for condition=Available=True
 
 # Install Flux with toleration to run controllers on the real node
@@ -26,17 +28,25 @@ kubectl create ns flux
 # make sure flux components get installed, with additional components
 flux install --components-extra="image-reflector-controller,image-automation-controller" --toleration-keys=realnode
 
+# Add label to allow cluster-services workloads to select this node
+kubectl label node "$name-control-plane" Project="end-to-end-tests"
 # taint the current node to not schedule workloads by default
 kubectl taint node "$name-control-plane" realnode=true:NoSchedule
 # Install kwok nodes to run "workloads" on
 kubectl apply -f $tests_path/kwok/1_large_simulated_node.yaml
+kubectl apply -f $tests_path/kwok/4_smaller_simulated_nodes.yaml
 
 
 
 # Preinstall
 # some components require a secret ahead of deployment
-kubectl create ns monitoring
+kubectl create ns victoriametrics
+kubectl create secret generic alerts-urls -n victoriametrics --from-literal=slack-api-url=none --from-literal=healthchecks-io-url=none
 # Install cluster stuff and wait
 flux create source git flux-system --url="$repo" --branch="$branch"
 flux create kustomization flux-system --source=flux-system --path="$test_kustomization_path"
 kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=1m -n flux-system flux-system
+# Force reconcile of all kustomizations
+flux reconcile kustomization crds
+flux reconcile kustomization system
+# flux reconcile kustomization deployments
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,12 +2,12 @@
 set -e
 
 tests_path="./tests"
-while IFS=$' ' read -r resource state namespace name ; do
-    if [[ "${resource:0:1}" == "#" ]]; then
+while IFS=$' ' read -r timeout resource state namespace name ; do
+    if [[ "${timeout:0:1}" == "#" ]]; then
         continue;
     fi
-    if [[ $resource == "" ]]; then
+    if [[ $timeout == "" ]]; then
         continue;
     fi
-    kubectl wait "${resource}" --for=condition=${state} --timeout=5m -n ${namespace} ${name}
+    kubectl wait "${resource}" --for=condition=${state} --timeout=${timeout} -n ${namespace} ${name}
 done < "$tests_path/resources_to_test.txt"
