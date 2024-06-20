f4a3a21efa855ff981d099801e9b087450ca4e2a
Scott Aubrey <scott@aubrey.org.uk>
2024-06-20 17:13:44 +0100

    Upgrade CSI snapshotter CRDs to v7.0.1 (mostly just install additional CRDS that were missing)

--- crds/csi-external-snapshotter/update.sh
+++ crds/csi-external-snapshotter/update.sh
@@ -10,9 +10,9 @@ fi
 
 cd $(dirname $0)
 
-# curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/groupsnapshot.storage.k8s.io_volumegroupsnapshotclasses.yaml
-# curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/groupsnapshot.storage.k8s.io_volumegroupsnapshotcontents.yaml
-# curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/groupsnapshot.storage.k8s.io_volumegroupsnapshots.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/groupsnapshot.storage.k8s.io_volumegroupsnapshotclasses.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/groupsnapshot.storage.k8s.io_volumegroupsnapshotcontents.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/groupsnapshot.storage.k8s.io_volumegroupsnapshots.yaml
 curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
 curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
 curl -O https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$1/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
