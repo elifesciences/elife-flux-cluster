6a2e2a95f328122062527176bca06b8278c35eed
Scott Aubrey <scott@aubrey.org.uk>
2024-09-19 07:59:49 +0100

    deploy(crds/gateway): deploy gateway v1 APIs elifesciences/issues#8934

---
+++ crds/gateway/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a gateway release version to get the CRDs'
+    echo "e.g. \`$0 1.0.0\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -L kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v$1/standard-install.yaml > gateway-api.crd.yaml
