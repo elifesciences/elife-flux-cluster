9bb6ba0909aecf2f07b36d58508d06654ed0b9c8
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-29 09:20:16 +0100

    Add route53 CRDs elifesciences/issues#9262

---
+++ crds/ack-controllers/route53-controller/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# As script to
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a route53-controller helm chart git reference to get the CRDs'
+    echo "e.g. \`$0 1.0.1\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/route53-controller/refs/tags/v$1/config/crd/bases/route53.services.k8s.aws_healthchecks.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/route53-controller/refs/tags/v$1/config/crd/bases/route53.services.k8s.aws_hostedzones.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/route53-controller/refs/tags/v$1/config/crd/bases/route53.services.k8s.aws_recordsets.yaml
