df290e12efd2367fdfaa2aedad88e065dccfdd7a
Scott Aubrey <scott@aubrey.org.uk>
2025-05-22 14:44:03 +0100

    deploy(ack-controllers/cloudfront): Deploy CloudFront ACK controller https://github.com/elifesciences/issues/issues/9283

---
+++ crds/ack-controllers/cloudfront-controller/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# As script to
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a cloudfront-controller helm chart git reference to get the CRDs'
+    echo "e.g. \`$0 1.0.1\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_cachepolicies.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_distributions.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_functions.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_originaccesscontrols.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_originrequestpolicies.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_responseheaderspolicies.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/cloudfront-controller/refs/tags/v$1/config/crd/bases/cloudfront.services.k8s.aws_vpcorigins.yaml
