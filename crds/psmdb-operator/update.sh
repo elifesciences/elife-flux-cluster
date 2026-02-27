990b2d9a5b7c7dccf37878901bfd8c78a560bb47
Scott Aubrey <scott@aubrey.org.uk>
2026-02-27 07:36:27 +0000

    deploy(crds/psmdb-operator): Update CRDs from new Crds helm chart

--- crds/psmdb-operator/update.sh
+++ crds/psmdb-operator/update.sh
@@ -10,4 +10,6 @@ fi
 
 cd $(dirname $0)
 
-curl -sO https://raw.githubusercontent.com/percona/percona-helm-charts/psmdb-operator-$1/charts/psmdb-operator/crds/crd.yaml
+curl -sO https://raw.githubusercontent.com/percona/percona-helm-charts/refs/tags/psmdb-operator-crds-$1/charts/psmdb-operator-crds/templates/perconaservermongodbbackups.psmdb.percona.com.yaml
+curl -sO https://raw.githubusercontent.com/percona/percona-helm-charts/refs/tags/psmdb-operator-crds-$1/charts/psmdb-operator-crds/templates/perconaservermongodbrestores.psmdb.percona.com.yaml
+curl -sO https://raw.githubusercontent.com/percona/percona-helm-charts/refs/tags/psmdb-operator-crds-$1/charts/psmdb-operator-crds/templates/perconaservermongodbs.psmdb.percona.com.yaml
